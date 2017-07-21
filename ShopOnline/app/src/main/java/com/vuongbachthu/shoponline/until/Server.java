076e3c9a8d56e7a6687e3fbc6194aa4c9dffc5b6
vuongbachthu <vuongbachthu@gmail.com>
2017-07-22 02:05:07 +0700

    Giai doan 3

---
+++ ShopOnline/app/src/main/java/com/vuongbachthu/shoponline/until/Server.java
@@ -0,0 +1,16 @@
+package com.vuongbachthu.shoponline.until;
+
+/**
+ * Created by vuong_000 on 7/11/2017.
+ */
+
+public class Server {
+    public static String localhost = "192.168.1.86:8080";
+    public static String urlLoaisp = "http://" + localhost + "/server/main/getloaisp.php";
+    public static String url_img = "http://" + localhost;
+    public static String url_sanphammoinhat = "http://" + localhost + "/server/main/getsanphammoinhat.php";
+
+    // List Dien Thoai
+    public static String url_sanpham_dienthoai = "http://" + localhost + "/server/main/getsanpham.php?page=";
+
+}
